e69e9ed0de5db6c39556a3dd57bd759feebe4a5b
agildw <yudistiraagil24@gmail.com>
2021-10-25 19:59:35 +0700

    added posted date

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cheerio = require('cheerio');
 
 axios.get('https://194.163.183.129/')
     .then(response => {
+        console.log('Last Update')
         const $ = cheerio.load(response.data);
         $('.post-show').first().find('[itemscope=itemscope]').each((i, item) => {
-            // let hasil = $(`div.thumb a`).attr('href');
-            // console.log(i)
             const targetUrl = $('a', item).attr('href');
-            // console.log($(item).html())
             console.log(targetUrl)
             const titles = $('a', item).attr('title');
             console.log(titles)
+            const postDate = $('div.dtla span', item).last().text().replace(' ', '');
+            console.log(postDate)
+            console.log('[=====]\n')
         })
     })
